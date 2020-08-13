cd08b2d92c400c62dce2e22372e26e3967136dc7
Urmila Unni <urmila8874@gmail.com>
2020-08-13 23:12:14 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,22 @@ import React from 'react';
 
 function App() {
   return (
-    <div>
-      <header className="App-header">
-       Phone Directory
-      </header>
+    <div> {
+  /*
+     <div className="header">
+      Phone Directory
+      
     </div>
+    <button>Add</button>
+    <div>
+      <span>Name</span>
+      <span>Phone </span>
+    </div> */}
+    <label htmlFor="name">Name: </label>
+    <input id="name" type="text" placeholder="Type here"  defaultValue="Urmila"></input>
+
+    </div>
+
   );
 }
 
